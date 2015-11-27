9d004ad276737eff5a433aea3f4720b216908495
Selman AY <selmanay@gmail.com>
2015-11-27 16:01:36 +0200

    ExecutionKey logging implemented.

---
+++ 19 Jul 2015/build.ps1
@@ -0,0 +1,35 @@
+﻿$template = (Get-Content .\MaintenanceSolution_Template.sql)
+
+$scriptFile = "MaintenanceSolution.sql"
+if(Test-Path -Path $scriptFile)
+{
+    Clear-Content -Path $scriptFile 
+}
+
+foreach($t_line in $template)
+{
+    if($t_line -eq "	PRINT '[[CommandLog_TableCreation_Script]]'")
+    {
+        (Get-Content -Path CommandLog.sql) | ForEach-Object -Process { Add-Content -Path $scriptFile -Value $_ }
+    }
+    elseif($t_line -eq "PRINT '[[CommandExecute_SPCreation_Script]]'")
+    {
+        (Get-Content -Path CommandExecute.sql) | ForEach-Object -Process { Add-Content -Path $scriptFile -Value $_ }
+    }
+    elseif($t_line -eq "PRINT '[[DatabaseBackup_SPCreation_Script]]'")
+    {
+        (Get-Content -Path DatabaseBackup.sql) | ForEach-Object -Process { Add-Content -Path $scriptFile -Value $_ }
+    }
+    elseif($t_line -eq "PRINT '[[DatabaseIntegrityCheck_SPCreation_Script]]'")
+    {
+        (Get-Content -Path DatabaseIntegrityCheck.sql) | ForEach-Object -Process { Add-Content -Path $scriptFile -Value $_ }
+    }
+    elseif($t_line -eq "PRINT '[[IndexOptimize_SPCreation_Script]]'")
+    {
+        (Get-Content -Path IndexOptimize.sql) | ForEach-Object -Process { Add-Content -Path $scriptFile -Value $_ }
+    }
+    else
+    {
+        Add-Content -Path $scriptFile -Value $t_line
+    }
+}
